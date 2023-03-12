3aedd9f0e9e879e8087833e21b867ef9b9699b9d
Lera Marshal <marshalmarshal777@gmail.com>
2023-03-12 14:49:47 +0100

    BankAccount using AtomicInteger

---
+++ HW/HW_20230312/src/main/java/BankAccountManager.java
@@ -0,0 +1,38 @@
+public class BankAccountManager {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+
+        new Thread(() -> {
+            while (true) {
+                bankAccount.deposit(100);
+                try {
+                    Thread.sleep(1900);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            while (true) {
+                bankAccount.withdraw(100);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        synchronized (bankAccount) {
+            while (true) {
+                System.out.println(bankAccount.getSum());
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
