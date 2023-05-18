2b66300343eaea1178dbe0c681f7859172e80424
Lera Marshal <marshalmarshal777@gmail.com>
2023-05-18 14:14:00 +0200

    Java 10 tasks low lvl

--- HW/HW_20230518/src/main/java/Task2.java
+++ HW/HW_20230518/src/main/java/Task2.java
@@ -30,18 +30,27 @@ public class Task2 {
 
     // Отсортировать массив по возрастанию
     public static void sortAsc(int[] arr) {
-//        int tmp = 0;
+        int tmp = 0;
         int len = arr.length;
 
-        for (int i = 0; i < len - 1; i++) {
-            for (int j = 0; j < len - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
+//        for (int i = 0; i < len - 1; i++) {
+//            for (int j = 0; j < len - i - 1; j++) {
+//                if (arr[j] > arr[j + 1]) {
+//                    int tmp = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = tmp;
+//                }
+//            }
+//        }
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (arr[i] < arr[j]) {
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
                 }
             }
-
         }
 
 //        Arrays.sort(arr);
